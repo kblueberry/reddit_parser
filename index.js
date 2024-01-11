a54382f3bce60c3297970c64c546323c99be1dec
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-01-11 18:34:40 +0200

    get all posts links by time-to-time scrolling

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  let allLinks = [];
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
   const page = await browser.newPage();
   await page.goto("https://www.reddit.com/r/Breadit/");
+  await getLinks(page, allLinks);
+  await scrollAndFind(page, allLinks);
+  await page.waitForTimeout(2000);
+  console.log("all links", allLinks);
+
+  await browser.close();
+})();
+
+async function scrollAndFind(page, allLinks) {
+  const distance = 100;
+  const delay = 100;
+  while (
+    await page.evaluate(
+      () =>
+        document.scrollingElement.scrollTop + window.innerHeight <
+        document.scrollingElement.scrollHeight
+    )
+  ) {
+    await page.evaluate((y) => {
+      document.scrollingElement.scrollBy(0, y);
+      console.log("scrolled");
+    }, distance);
+    await getLinks(page, allLinks);
+    await page.waitForTimeout(delay);
+  }
+}
+
+async function getLinks(page, allLinks) {
   await page.waitForSelector('a[slot="full-post-link"]');
 
   const postLinks = await page.$$eval('a[slot="full-post-link"]', (links) =>
     links.map((link) => link.href)
   );
-  console.log("hrefs", postLinks);
-
-  await browser.close();
-})();
+  allLinks = allLinks.concat(...postLinks);
+}
