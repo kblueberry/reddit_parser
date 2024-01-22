17b7b87f179eb78157146a4bdc507e7a33f938c2
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-01-22 19:55:44 +0200

    get all links when searching and scrolling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const puppeteer = require("puppeteer");
   await page.goto("https://www.reddit.com/r/Breadit/search/?q=rye%20bread");
 
   // search and write results to .txt file
-  await searchAndSave2(page);
+  await searchAndSave(page);
 
   // await getLinks(page, allLinks);
   // await scrollAndFind(page, allLinks);
@@ -14,62 +14,33 @@ const puppeteer = require("puppeteer");
   await browser.close();
 })();
 
-async function searchAndSave2(page) {
+async function searchAndSave(page) {
   let allLinks = [];
 
   const searchResultsLinks = await page.$$eval(
     'body >>> a[data-testid="post-title"]',
-    (elements) => elements.map((el) => el.href)
+    (elements) =>
+      elements.map((el) => {
+        return {
+          href: el.href,
+          positionY: el.getBoundingClientRect().y,
+        };
+      })
   );
 
-  allLinks = allLinks.concat(searchResultsLinks);
-  console.log("links results", allLinks, allLinks.length);
-  // await getResultsOnScroll(
-  //   page,
-  //   allLinks,
-  //   'body >>> a[data-testid="post-title"]'
-  // );
+  allLinks = [...allLinks].concat(...searchResultsLinks);
+
+  await getResultsOnScroll(
+    page,
+    allLinks,
+    searchResultsLinks[0].positionY,
+    searchResultsLinks[searchResultsLinks.length - 1].positionY
+  );
 }
 
-async function searchAndSave(page) {
-  const inputSelector = "body >>> input[placeholder='Search in r/Breadit']";
+async function getResultsOnScroll(page, allLinks, firstEl, lastEl) {
+  const distance = lastEl - firstEl;
 
-  const elementHandle = await page.waitForSelector(inputSelector);
-  // await page.focus(inputSelector);
-  const searchButton = await page.waitForSelector("body >>> span.leadingIcon");
-  console.log(searchButton);
-  await page.evaluate((el) => el.click(), searchButton);
-
-  // await page.$eval(inputSelector, (el) => (el.value = "rye"));
-  await page.keyboard.type("Enter");
-
-  console.log("input: ", elementHandle);
-  // console.log(
-  //   "value after type",
-  //   await page.evaluate((element) => element.value, elementHandle)
-  // );
-
-  // const searchResults = await page.$('body >>> a[data-testid="post-title"]');
-  // console.log("page results: ", searchResults);
-
-  // console.log("search res: ", searchResults);
-  // await page.type(searchInputSelector, "rye sourdough");
-
-  // const resultsOfSearch = await page.waitForSelector(
-  //   'a[data-testid="post-title"]'
-  // );
-  // console.log("res: ", resultsOfSearch);
-  // let allLinks = [];
-
-  // await getResultsOnScroll(
-  //   page,
-  //   allLinks,
-  //   'body >>> a[data-testid="post-title"]'
-  // );
-}
-
-async function getResultsOnScroll(page, results, resultsSelector) {
-  const distance = await page.evaluate(() => window.innerHeight);
   const delay = 1000;
   while (
     await page.evaluate(
@@ -81,7 +52,7 @@ async function getResultsOnScroll(page, results, resultsSelector) {
     await page.evaluate((y) => {
       document.scrollingElement.scrollBy(0, y);
     }, distance);
-    await getLinks(page, results, resultsSelector);
+    await getLinks(page, allLinks, 'body >>> a[data-testid="post-title"]');
     await page.waitForTimeout(delay);
   }
 }
@@ -91,9 +62,12 @@ async function getLinks(page, results, selector) {
 
   const postLinks = await page.$$eval(selector, (links) =>
     links.map((link) => {
-      return { href: link.href };
+      return {
+        href: link.href,
+        positionY: link.getBoundingClientRect().y,
+      };
     })
   );
-  console.log("results", results, results.length);
   results = results.concat(...postLinks);
+  console.log("results", results, results.length);
 }
